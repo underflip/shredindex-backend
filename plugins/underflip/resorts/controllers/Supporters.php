26be6915541249d28565ee18f473b39b1519a368
Jackson Darlow <jackson.e.darlow@gmail.com>
2021-05-03 20:27:58 +1200

    Integrate global data with CMS via GraphQL https://trello.com/c/qw7b52JS

---
+++ plugins/underflip/resorts/controllers/Supporters.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Underflip\Resorts\Controllers;
+
+use Backend\Behaviors\FormController;
+use Backend\Behaviors\ListController;
+use Backend\Behaviors\ReorderController;
+use Backend\Classes\Controller;
+use BackendMenu;
+use System\Classes\SettingsManager;
+
+/**
+ * A controller to handle create, update and delete of Supporters, even thought
+ * {@see Settings} renders its list
+ */
+class Supporters extends Controller
+{
+    /**
+     * @var array Extensions implemented by this controller.
+     */
+    public $implement = [
+        ListController::class,
+        FormController::class,
+        ReorderController::class,
+    ];
+
+    /**
+     * @var array
+     */
+    public $listConfig = 'config_list.yaml';
+
+    /**
+     * @var array
+     */
+    public $formConfig = 'config_form.yaml';
+
+    /**
+     * @var string
+     */
+    public $reorderConfig = 'config_reorder.yaml';
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->makeLists();
+
+        BackendMenu::setContext('October.System', 'system', 'settings');
+        SettingsManager::setContext('October.System', 'shredindex_supporters');
+    }
+}
