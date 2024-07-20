9ac44dc48709e3c48a5364858d638fab6ee48063
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2024-07-20 19:16:48 +1000

    Revert old merge commit changes (#59)

--- plugins/underflip/resorts/updates/create_types_table.php
+++ plugins/underflip/resorts/updates/create_types_table.php
@@ -20,6 +20,7 @@ class CreateTypesTable extends Migration
             $table->integer('unit_id')->nullable();
             $table->string('icon')->nullable();
             $table->decimal('max_value')->nullable();
+            $table->integer('type_group_id')->nullable();
         });
     }
 
